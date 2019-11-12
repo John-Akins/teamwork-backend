77a8e0dfd8f9e4230d46330a080eb53202c9fb67
John Akins <JohnAkins.dev@gmail.com>
2019-11-12 11:36:06 +0100

    chore(ci/cd):fixes bug on test/api/create-user [continues #9]

--- src/test/api/create-user.js
+++ src/test/api/create-user.js
@@ -6,146 +6,149 @@ import app from '../../app'
 chai.use(chatHttp);
 const { expect } = chai;
 
-describe("admin create employee", () => {
+describe("create user", () => {
 
-	const adminSecrets = {}
-	before((done) => {
-		chai.request(app)
-		.post('/api/v1/auth/signin')
-		.set('Accept', 'application/json')
-		.send({
-			email: "lovelace@gmail.com",
-			password: "password"
-		})
-		.end((error, response) => {
-			adminSecrets.data = response.body.data
-			console.log("user signin")
-			console.log(adminSecrets.data)
-			done();
-		});
-	})
+	describe("admin create employee", () => {
 
-	describe("input existing email", () => {
-		const data = {}
+		const adminSecrets = {}
 		before((done) => {
 			chai.request(app)
-			.post('/api/v1/auth/create-user')
-			.set({
-				'Accept': 'application/json',
-				"Authorization": `token: ${adminSecrets.data.token}`
-			})
+			.post('/api/v1/auth/signin')
+			.set('Accept', 'application/json')
 			.send({
-				userId: adminSecrets.data.userId,
-				firstName: "akins",
-				lastName: "akin",
+				email: "lovelace@gmail.com",
+				password: "password"
+			})
+			.end((error, response) => {
+				adminSecrets.data = response.body.data
+				done();
+			});
+		})
+
+		describe("input existing email", () => {
+			const data = {}
+			before((done) => {
+				chai.request(app)
+				.post('/api/v1/auth/create-user')
+				.set({
+					'Accept': 'application/json',
+					"Authorization": `token: ${adminSecrets.data.token}`
+				})
+				.send({
+					userId: adminSecrets.data.userId,
+					firstName: "akins",
+					lastName: "akin",
+					email: "turan@gmail.com",
+					address: "akins street", 
+					password: "dfjdskjfsk",
+					gender: "male",
+					jobRole: "Engineer",
+					department: "IT",
+					isAdmin: true
+				})
+				.end((error, response) => {
+					data.status = response.statusCode
+					data.body = response.body
+					done();
+				});
+			})
+
+			it("should return 402 status code", () => {
+				expect(data.status).to.equal(402)
+			})
+			it("should return relevant error message", () => {
+				expect(data.body.error).eql("this email already exists")
+			})
+		})
+
+		describe("empty field(s)/ wrong input format", () => {
+			const data = {}
+			before((done) => {
+				chai.request(app)
+				.post('/api/v1/auth/create-user')
+				.set({
+					'Accept': 'application/json',
+					"Authorization": `token: ${adminSecrets.data.token}`
+				})
+				.send({
+					userId: adminSecrets.data.userId,
+					firstName: "",
+					lastName: "",
+					email: "",
+					address: "", 
+					password: "",
+					gender: "",
+					jobRole: "",
+					department: "",
+					isAdmin: false
+				})
+				.end((error, response) => {
+					data.status = response.statusCode
+					data.body = response.body
+					done();
+				});
+			})
+
+			it("should return 422 status code", () => {
+				expect(data.status).to.equal(422)
+			})
+			it("should return an error array", () => {
+				expect(data.body.error).to.be.an('array')
+			})
+		})
+	})
+
+	describe("user create employee", () => {
+		const userSecrets = {}	
+
+		before((done) => {
+			chai.request(app)
+			.post('/api/v1/auth/signin')
+			.set('Accept', 'application/json')
+			.send({
 				email: "turan@gmail.com",
-				address: "akins street", 
-				password: "dfjdskjfsk",
-				gender: "male",
-				jobRole: "Engineer",
-				department: "IT",
-				isAdmin: true
+				password: "password"
 			})
 			.end((error, response) => {
-				data.status = response.statusCode
-				data.body = response.body
+				userSecrets.data = response.body.data
 				done();
 			});
 		})
 
-		it("should return 402 status code", () => {
-			expect(data.status).to.equal(402)
-		})
-		it("should return relevant error message", () => {
-			expect(data.body.error).eql("this email already exists")
-		})
-	})
-
-	describe("empty field(s)/ wrong input format", () => {
-		const data = {}
-		before((done) => {
-			chai.request(app)
-			.post('/api/v1/auth/create-user')
-			.set({
-				'Accept': 'application/json',
-				"Authorization": `token: ${adminSecrets.data.token}`
+		describe("user not an admin", () => {
+			const data = {}
+			before((done) => {
+				chai.request(app)
+				.post('/api/v1/auth/create-user')
+				.set({
+					'Accept': 'application/json',
+					"Authorization": `token: ${userSecrets.data.token}`
+				})
+				.send({
+					userId: userSecrets.data.userId,
+					firstName: "sola",
+					lastName: "akin",
+					email: "akins@gmail.com",
+					address: "akins street", 
+					password: "dfjdskjfsk",
+					gender: "male",
+					jobRole: "Engineer",
+					department: "IT",
+					isAdmin: false,
+				})
+				.end((error, response) => {
+					data.status = response.statusCode
+					data.body = response.body
+					done();
+				});
 			})
-			.send({
-				userId: adminSecrets.data.userId,
-				firstName: "",
-				lastName: "",
-				email: "",
-				address: "", 
-				password: "",
-				gender: "",
-				jobRole: "",
-				department: "",
-				isAdmin: false
+
+			it("should return 401 status code", () => {
+				expect(data.status).to.equal(401)
 			})
-			.end((error, response) => {
-				data.status = response.statusCode
-				data.body = response.body
-				done();
-			});
-		})
-
-		it("should return 422 status code", () => {
-			expect(data.status).to.equal(422)
-		})
-		it("should return an error array", () => {
-			expect(data.body.error).to.be.an('array')
-		})
-	})
-	
-	const userSecrets = {}	
-
-	before((done) => {
-		chai.request(app)
-		.post('/api/v1/auth/signin')
-		.set('Accept', 'application/json')
-		.send({
-			email: "turan@gmail.com",
-			password: "password"
-		})
-		.end((error, response) => {
-			userSecrets.data = response.body.data
-			done();
-		});
-	})
-
-	describe("user is not an admin", () => {
-		const data = {}
-		before((done) => {
-			chai.request(app)
-			.post('/api/v1/auth/create-user')
-			.set({
-				'Accept': 'application/json',
-				"Authorization": `token: ${userSecrets.data.token}`
+			it("should return error relevant message", () => {
+				expect(data.body.error).eql("Elevated access rights required")
 			})
-			.send({
-				userId: userSecrets.data.userId,
-				firstName: "sola",
-				lastName: "akin",
-				email: "akins@gmail.com",
-				address: "akins street", 
-				password: "dfjdskjfsk",
-				gender: "male",
-				jobRole: "Engineer",
-				department: "IT",
-				isAdmin: false,
-			})
-			.end((error, response) => {
-				data.status = response.statusCode
-				data.body = response.body
-				done();
-			});
 		})
-
-		it("should return 401 status code", () => {
-			expect(data.status).to.equal(401)
-		})
-		it("should return error relevant message", () => {
-			expect(data.body.error).eql("Elevated access rights required")
-		})
-	})
+	})	
 })
