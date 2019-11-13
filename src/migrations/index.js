df599260b783e9534f1fdc64eb5e9ce160013f9c
John Akins <JohnAkins.dev@gmail.com>
2019-11-13 13:00:13 +0100

    chore(CI/CD)debugs migration/index.js (continues #9)

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -1,5 +1,59 @@
 import db from "../db"
 
+dbMigration.tablesAndQueries = [
+    {
+        table: "users", 
+        query: 'CREATE TABLE users ( "userId" bigint NOT NULL, "firstName" character varying(30) NOT NULL, "lastName" character varying NOT NULL, email character varying NOT NULL, address character varying NOT NULL, password character varying NOT NULL, gender character varying NOT NULL, "jobRole" character varying NOT NULL, department character varying NOT NULL, "isAdmin" boolean NOT NULL, "isNewAccount" boolean DEFAULT true NOT NULL )'
+    },
+
+    {
+        table: "articles",
+        query: 'CREATE TABLE articles ( title character(100) NOT NULL, "articleId" bigint NOT NULL, "createdOn" date NOT NULL, "createdBy" character varying(20) NOT NULL, article character(1000) NOT NULL )'
+    },
+
+    {
+        table: "feedComments",
+        query: 'CREATE TABLE "feedComments" ( "feedId" bigint NOT NULL, "commentId" bigint NOT NULL, "feedType" character(20) NOT NULL, comment character(500) NOT NULL, "commentOn" date NOT NULL, "commentBy" bigint NOT NULL, "isFlagged" boolean )'
+    },
+
+    {
+        table: "feedFlags",
+        query: 'CREATE TABLE "feedFlags" ( "flagId" bigint NOT NULL, "feedId" bigint NOT NULL, "feedType" character(20) NOT NULL, "flaggedOn" date, "flaggedBy" character(20) )'
+    },
+
+    {
+        table: "gifs",
+        query: 'CREATE TABLE gifs ( "gifId" bigint NOT NULL, title character(100) NOT NULL, "imageUrl" character(100) NOT NULL, "createdOn" date NOT NULL, "createdBy" character(20)[] NOT NULL )'
+    }
+]
+
+dbMigration.hasCreatedTables = false
+
+dbMigration.createTablesIfNotExists = () => {
+        const tablesAndQueries = dbMigration.tablesAndQueries
+        for (let i = 0; i < tablesAndQueries.length; i++) 
+        {
+            dbMigration.hasCreatedTables = (i === 0) ? true : dbMigration.hasCreatedTables
+            const table = tablesAndQueries[i].table
+            const tableQuery = tablesAndQueries[i].query
+
+                db.query(tableQuery)
+                .then(() => {
+                    console.log("table created")
+                    dbMigration.hasCreatedTables = dbMigration.hasCreatedTables && true
+                })
+                .catch( () => {
+                    console.log("table not created")
+                    dbMigration.hasCreatedTables = dbMigration.hasCreatedTables && error
+                })
+            console.log("dbMigration.hasCreatedTables")
+            console.log(dbMigration.hasCreatedTables)
+        }
+        return dbMigration.hasCreatedTables
+}
+/*
+import db from "../db"
+
 const dbMigration = {}
 
 dbMigration.tableExists = (table) => {
@@ -58,7 +112,7 @@ dbMigration.createTables = () => {
         }
         return true
 }
-
+*/
 dbMigration.dummyQueries = [
 
     {
