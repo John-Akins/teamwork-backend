1760ac9de3a48962d7e98a310202841a6be09089
John Akins <JohnAkins.dev@gmail.com>
2019-11-14 13:47:44 +0100

    rf(auth)updates middleware/auth.js (continues #9)

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -43,11 +43,11 @@ auth.adminOnly = (req, res, next) => {
 			}
 		}
 		catch(e) {
-			return	responseUtility.error(res, 401, "Unauthorized request")
+			return	responseUtility.error(res, 401, "Unauthorized request 2")
 		}
 	}
 	catch(e) {
-		return	responseUtility.error(res, 401, "Unauthorized request")
+		return	responseUtility.error(res, 401, "Unauthorized request 2")
 	}
 }
 
