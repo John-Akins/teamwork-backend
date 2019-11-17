4e24edd04cbe59942f0068076068ab91d29a8f2f
John Akins <JohnAkins.dev@gmail.com>
2019-11-17 10:54:01 +0100

    feature(articles)debugs db migration scripts (continues #28)

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -74,7 +74,7 @@ dbMigration.dummyQueries = [
 
     {
         table: "articles",
-        query: 'INSERT INTO articles ("title", "articleId", "createdOn", "createdBy", "article") values  (\'Ada Lovelace\', 10001, \'2019-10-12\',	10002, \'A computer science fairy tale\')',
+        query: 'INSERT INTO articles ("title", "articleId", "createdOn", "createdBy", "article") values  (\'Ada Lovelace\', 10001, \'2019-10-12\',	10001, \'A computer science fairy tale\')',
 
     },
     {
@@ -88,7 +88,7 @@ dbMigration.dummyQueries = [
     {
         table: "users",
         query: 'INSERT INTO "users" ("userId", "firstName", "lastName", "email", "address", "password", "gender", "jobRole", "department", "isAdmin", "isNewAccount") values (10002, \'Ada\', \'Turan\', \'turan@gmail.com\', \'LOvelace street\', \'$2b$10$dTlK9RWsDFxj0jvAARftqeonxRuBVTQVKpsbvk9tt.MsFcjnTjpxa\', \'female\',	\'Software Engineer\', \'IT\',	FALSE,	FALSE)'
-    },
+    }
 
 ]
 
