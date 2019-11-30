414c4e86fd31e5b4e50dad8307f0435d1632fb80
John Akins <JohnAkins.dev@gmail.com>
2019-11-30 22:18:09 +0100

    updated and gitignored config credentials

--- src/config/db.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  development: 'postgres://qulzkjox:QjAK5pvJYcP2OyeQlpRzRYWMYVJhSA5D@manny.db.elephantsql.com:5432/qulzkjox',
-  test: 'postgres://pllohbki:GUA79dfsfnJrekrIbJ4aA-LZCJQlfGsd@isilo.db.elephantsql.com:5432/pllohbki',
-};
