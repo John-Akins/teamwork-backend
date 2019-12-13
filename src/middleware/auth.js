cf890e5be9976482c28a998dc29ae83c554ffdfa
John Akins <JohnAkins.dev@gmail.com>
2019-12-13 10:31:17 +0100

    updates auth middleware [finished #73]

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,11 +7,89 @@ const tokenSecret = '$hdsJmzjQ7,E.m2y$12$1iTvLIHS60iMROUjADnu8tdiUguselTrWjDo6Sx
 auth.allUsers = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    const userIdMatch = req.headers.authorization.split(' ')[3];import jwt from 'jsonwebtoken';
+    import responseUtility from '../utilities/responseUtility';
+    
+    const auth = {};
+    const tokenSecret = '$hdsJmzjQ7,E.m2y$12$1iTvLIHS60iMROUjADnu8tdiUguselTrWjDo6SxVf';
+    
+    auth.allUsers = (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(' ')[1];
+        const userIdMatch = req.headers.authorization.split(' ')[3];
+        const decodedToken = jwt.verify(token, tokenSecret);
+        const { userId } = decodedToken;
+    
+        try {
+          if (userId !== userIdMatch) {
+            throw new Error('Invalid user ID'+userIdMatch);
+          } else {
+            next();
+          }
+        } catch (e) {
+          return responseUtility.error(res, 401, e.message);
+        }
+      } catch (e) {
+        return responseUtility.error(res, 401, 'Unauthorized request'+e);
+      }
+      return false;
+    };
+    
+    auth.adminOnly = (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(' ')[1];
+        const userIdMatch = req.headers.authorization.split(' ')[3];
+
+        const decodedToken = jwt.verify(token, tokenSecret);
+        const { userId, isAdmin } = decodedToken;
+    
+        try {
+          if (userId !== userIdMatch) {
+            throw new Error('Invalid user ID');
+          }
+          if (isAdmin !== true) {
+            throw new Error('Elevated access rights required');
+          } else {
+            next();
+          }
+        } catch (e) {
+          return responseUtility.error(res, 401, e.message);
+        }
+      } catch (e) {
+        return responseUtility.error(res, 401, 'Unauthorized request');
+      }
+      return false;
+    };
+    
+    auth.userIdMatchesAuthorId = (req, res, next) => {
+      try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, tokenSecret);
+        const { userId } = decodedToken;
+    
+        try {
+          if (userId && parseInt(userId, 10) !== parseInt(req.body.authorId, 10)) {
+            throw new Error('Only admin or account owner can edit/delete this feed, want to flag as inappropriate?');
+          } else {
+            next();
+          }
+        } catch (e) {
+          return responseUtility.error(res, 401, e.message);
+        }
+      } catch (e) {
+        return responseUtility.error(res, 401, 'Unauthorized request');
+      }
+      return false;
+    };
+    
+    export default auth;
+    
     const decodedToken = jwt.verify(token, tokenSecret);
+    const { userId } = decodedToken;
+
     try {
-      const { userId } = decodedToken;
-      if (req.headers.userId && req.headers.userId !== userId) {
-        throw new Error('Invalid user ID');
+      if (userId !== userIdMatch) {
+        throw new Error('Invalid user ID'+userIdMatch);
       } else {
         next();
       }
@@ -19,19 +97,21 @@ auth.allUsers = (req, res, next) => {
       return responseUtility.error(res, 401, e.message);
     }
   } catch (e) {
-    return responseUtility.error(res, 401, 'Unauthorized request');
+    return responseUtility.error(res, 401, 'Unauthorized request'+e);
   }
   return false;
 };
 
 auth.adminOnly = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1]
+    const userIdMatch = req.headers.authorization.split(' ')[3];
+
     const decodedToken = jwt.verify(token, tokenSecret);
     const { userId, isAdmin } = decodedToken;
 
     try {
-      if (req.headers.userId && req.headers.userId !== userId) {
+      if (userId !== userIdMatch) {
         throw new Error('Invalid user ID');
       }
       if (isAdmin !== true) {
